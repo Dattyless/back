2850b8668f9dfbc096b338a129b033c1cf5b0e47
attyless <izanovic6@gmail.com>
2025-02-13 09:42:57 +0100

    me va a dar algo

--- dist/src/db-connection.js
+++ dist/src/db-connection.js
@@ -9,7 +9,7 @@ var pg_1 = require("pg");
 //   port: 5432, // default Postgres port
 //   database: 'BD_proyecto_final'
 // });
-var connectionString = 'postgresql://proyecto_final_24_25_rwyr_user:7ccTb7oSW7EJ2Ok8FCsIDnoc3ao4bmpK@dpg-cum64vd6l47c7395bq70-a.oregon-postgres.render.com/proyecto_final_24_25_rwyr';
+var connectionString = 'postgresql://proyecto_final_24_25_rwyr_user:7ccTb7oSW7EJ2Ok8FCsIDnoc3ao4bmpK@dpg-cum64vd6l47c7395bq70-a/proyecto_final_24_25_rwyr';
 var pool = new pg_1.Pool({
     connectionString: connectionString,
 });
--- src/db-connection.ts
+++ src/db-connection.ts
@@ -8,7 +8,7 @@ import { Pool } from 'pg';
 //   database: 'BD_proyecto_final'
 // });
 
-const connectionString = 'postgresql://proyecto_final_24_25_rwyr_user:7ccTb7oSW7EJ2Ok8FCsIDnoc3ao4bmpK@dpg-cum64vd6l47c7395bq70-a.oregon-postgres.render.com/proyecto_final_24_25_rwyr';
+const connectionString = 'postgresql://proyecto_final_24_25_rwyr_user:7ccTb7oSW7EJ2Ok8FCsIDnoc3ao4bmpK@dpg-cum64vd6l47c7395bq70-a/proyecto_final_24_25_rwyr';
 
 const pool = new Pool({
   connectionString,
