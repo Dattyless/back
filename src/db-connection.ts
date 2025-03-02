77447b08bdd35634c8943d394e05c48fa964e933
Tu Nombre <izanovic6@gmail.com>
2025-03-02 16:46:36 +0100

    n

--- src/db-connection.ts
+++ src/db-connection.ts
@@ -1,9 +1,9 @@
 import { Pool } from 'pg';
 
+const connectionString = 'postgresql://proyecto_final_24_25_rwyr_user:7ccTb7oSW7EJ2Ok8FCsIDnoc3ao4bmpK@dpg-cum64vd6l47c7395bq70-a.oregon-postgres.render.com/proyecto_final_24_25_rwyr';
+
 const pool = new Pool({
-  connectionString:
-    'postgresql://proyecto_final_24_25_rwyr_user:7ccTb7oSW7EJ2Ok8FCsIDnoc3ao4bmpK@dpg-cum64vd6l47c7395bq70-a.oregon-postgres.render.com/proyecto_final_24_25_rwyr',
-  ssl: { rejectUnauthorized: false }, // 🔹 Render exige esto
+  connectionString,
 });
 
 export function query(text: string, params?: any[]) {
