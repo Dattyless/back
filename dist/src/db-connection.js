760b3b19ab60f79ab4b45e185114b1ada035ebfe
Tu Nombre <izanovic6@gmail.com>
2025-03-02 16:53:59 +0100

    x

--- dist/src/db-connection.js
+++ dist/src/db-connection.js
@@ -1,12 +1,15 @@
 "use strict";
 Object.defineProperty(exports, "__esModule", { value: true });
-exports.query = void 0;
+exports.query = query;
 var pg_1 = require("pg");
+// Cadena de conexión proporcionada
 var connectionString = 'postgresql://proyecto_final_24_25_rwyr_user:7ccTb7oSW7EJ2Ok8FCsIDnoc3ao4bmpK@dpg-cum64vd6l47c7395bq70-a.oregon-postgres.render.com/proyecto_final_24_25_rwyr';
 var pool = new pg_1.Pool({
     connectionString: connectionString,
+    ssl: {
+        rejectUnauthorized: false, // Esto desactiva la verificación estricta del certificado SSL.
+    },
 });
 function query(text, params) {
     return pool.query(text, params);
 }
-exports.query = query;
--- src/db-connection.ts
+++ src/db-connection.ts
@@ -1,11 +1,12 @@
 import { Pool } from 'pg';
 
+// Cadena de conexión proporcionada
 const connectionString = 'postgresql://proyecto_final_24_25_rwyr_user:7ccTb7oSW7EJ2Ok8FCsIDnoc3ao4bmpK@dpg-cum64vd6l47c7395bq70-a.oregon-postgres.render.com/proyecto_final_24_25_rwyr';
 
 const pool = new Pool({
   connectionString,
   ssl: {
-    rejectUnauthorized: false, // Esto es necesario para conexiones con certificados autofirmados
+    rejectUnauthorized: false, // Esto desactiva la verificación estricta del certificado SSL.
   },
 });
 
