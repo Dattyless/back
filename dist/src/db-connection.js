bb92cf6772f23e2d9316502f726bd184a4d2e3d0
Tu Nombre <izanovic6@gmail.com>
2025-03-02 15:33:58 +0100

    si

--- dist/src/db-connection.js
+++ dist/src/db-connection.js
@@ -2,7 +2,7 @@
 Object.defineProperty(exports, "__esModule", { value: true });
 exports.query = void 0;
 var pg_1 = require("pg");
-var connectionString = 'postgresql://proyecto_final_24_25_rwyr_user:7ccTb7oSW7EJ2Ok8FCsIDnoc3ao4bmpK@dpg-cum64vd6l47c7395bq70-a/proyecto_final_24_25_rwyr';
+var connectionString = 'postgresql://proyecto_final_24_25_rwyr_user:7ccTb7oSW7EJ2Ok8FCsIDnoc3ao4bmpK@dpg-cum64vd6l47c7395bq70-a.oregon-postgres.render.com/proyecto_final_24_25_rwyr';
 var pool = new pg_1.Pool({
     connectionString: connectionString,
 });
--- src/db-connection.ts
+++ src/db-connection.ts
@@ -1,6 +1,6 @@
 import { Pool } from 'pg';
 
-const connectionString = 'postgresql://proyecto_final_24_25_rwyr_user:7ccTb7oSW7EJ2Ok8FCsIDnoc3ao4bmpK@dpg-cum64vd6l47c7395bq70-a/proyecto_final_24_25_rwyr';
+const connectionString = 'postgresql://proyecto_final_24_25_rwyr_user:7ccTb7oSW7EJ2Ok8FCsIDnoc3ao4bmpK@dpg-cum64vd6l47c7395bq70-a.oregon-postgres.render.com/proyecto_final_24_25_rwyr';
 
 const pool = new Pool({
   connectionString,
