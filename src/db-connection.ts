e4ed89f781de020a6ba927db1ff119af3fcc3fda
Tu Nombre <izanovic6@gmail.com>
2025-03-02 16:33:05 +0100

    l

--- src/db-connection.ts
+++ src/db-connection.ts
@@ -3,7 +3,9 @@ import { Pool } from 'pg';
 const pool = new Pool({
   connectionString:
     'postgresql://proyecto_final_24_25_rwyr_user:7ccTb7oSW7EJ2Ok8FCsIDnoc3ao4bmpK@dpg-cum64vd6l47c7395bq70-a.oregon-postgres.render.com/proyecto_final_24_25_rwyr',
-  ssl: true, // 🔹 Esto habilita SSL sin usar rejectUnauthorized
+  ssl: {
+    rejectUnauthorized: false, // 🔹 Solo esto, sin "require"
+  },
 });
 
 export function query(text: string, params?: any[]) {
